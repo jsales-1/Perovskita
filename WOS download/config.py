e5a145c9283a1ad45b487b2983d530efb6837a33
J. David <jose23038@ilum.cnpem.br>
2025-02-28 16:40:14 -0300

    Delete WOS download directory

--- WOS download/config.py
+++
@@ -1,8 +0,0 @@
-CAFE = True  # se irá utilizar o cafe
-LOGIN = "jose23038@ilum.cnpem.br"  # cafe login
-SENHA = ""  # cafe senha
-SIGLA = "CNPEM"
-NOME_UNIVERSIDADE = "CNPEM - CENTRO NACIONAL DE PESQUISA EM ENERGIA E MATERIAIS"
-PESQUISA_AVANCADA = True
-PESQUISA = """TS=(*perovskit*) AND PY=(2019-2025)"""
-NOME_PASTA = "perovskites_parte2"  # nome da pasta para download
